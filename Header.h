288f9123921670ab63b216c4a41729467eec0ec0
Belbrob <48256582+Belbrob@users.noreply.github.com>
2019-03-06 00:08:39 +0200

    add_prototypes_of_funcs_instead_of_implementation)

--- Header.h
+++ Header.h
@@ -19,30 +19,5 @@ public:
 	void set_country_info(string str, int num);
 };
 
-void sort_marks(vector <Country> &Countries_list, int num) {
-	int *res = new int[10]{ 12, 10, 8, 7, 6, 5, 4, 3, 2, 1 };
-
-	for (int q = 0; q < num; q++) {
-		for (int i = 0; i < num; i++) {
-			for (int j = 0; j < num - 1; j++) {
-				if (Countries_list[j].mark[q] < Countries_list[j + 1].mark[q]) {
-					swap(Countries_list[j], Countries_list[j + 1]);
-				}
-			}
-		}
-
-		for (int i = 0; i < num; i++) {
-			Countries_list[i].result += (i >= 10) ? 0 : (res[i]);
-		}
-	}
-}
-
-void sort_result(vector <Country> &Countries_list, int num) {
-	for (int i = 0; i < num; i++) {
-		for (int j = 0; j < num - 1; j++) {
-			if (Countries_list[j].result < Countries_list[j + 1].result) {
-				swap(Countries_list[j], Countries_list[j + 1]);
-			}
-		}
-	}
-}
+void sort_marks(vector <Country> &Countries_list, int num);
+void sort_result(vector <Country> &Countries_list, int num);
