4701d150af44b496d079ca4d93c21599acf181c4
AlBersg <43479452+AlBersha@users.noreply.github.com>
2019-03-16 20:56:11 +0200

    Update StackHeader.h

--- Lab_3/StackHeader.h
+++ Lab_3/StackHeader.h
@@ -1,1 +1,18 @@
+#pragma once
+
+class Stack
+{
+	struct elem{
+		string symb;
+		elem *next;
+	};
+	elem *top;
+
+public:
+	Stack();
+	~Stack();
+	void push(string value);
+	string pop();
+	bool is_empty();
+};
 
