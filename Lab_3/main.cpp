7d0935bfadfbb0ce097b3a7ad09f6d2a60d06eae
AlBersg <43479452+AlBersha@users.noreply.github.com>
2019-03-19 12:47:26 +0200

    Update main.cpp

--- Lab_3/main.cpp
+++ Lab_3/main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
 #include <string>
+#include <ctime>
+//#include "Header.h"
 #include "StackHeader.h"
+#include "PolishIntertreterHeader.h"
 using namespace std;
 
-int main() {
+int main(int argc, const char *argv[]) {
 
+	string line;
+
+	for (int i = 1; i < argc; i++) {
+		line += argv[i];
+	}
+
+	line = toPostfix(line);
+	cout << "Postfix form: " << toPostfix(line) << endl;
+	cout << "Result = " << Interpreter(line) << endl;
+
+
+	system("pause");
 }
